93cf73b25ce9b1e986e6f20671a65c9c49bcc509
Shahed-BD71 <mdshahedbd71@gmail.com>
2021-04-01 20:18:22 +0600

    fix

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAwCF02v7oRQJn29oqFBO9UX_Ke_nbtRZ0",
+    authDomain: "burj-al-arab-auth-simple.firebaseapp.com",
+    projectId: "burj-al-arab-auth-simple",
+    storageBucket: "burj-al-arab-auth-simple.appspot.com",
+    messagingSenderId: "625887273527",
+    appId: "1:625887273527:web:46f5fccb09a8f919c415b2"
+};
+export default firebaseConfig;
